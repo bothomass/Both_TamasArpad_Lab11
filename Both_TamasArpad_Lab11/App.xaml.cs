302287442d5e2d5587eeb20e6f2585d81ef9fd32
Tamas Arpad Both <both.tamas.arpad@gmail.com>
2023-12-17 17:02:34 +0200

    Add project files.

---
+++ Both_TamasArpad_Lab11/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Both_TamasArpad_Lab11
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
