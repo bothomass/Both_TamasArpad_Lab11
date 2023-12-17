fb6a5cde59dbfca1df858bce4a4572b5614e8cfb
Tamas Arpad Both <both.tamas.arpad@gmail.com>
2023-12-17 18:05:35 +0200

    Completed Laborator11

--- Both_TamasArpad_Lab11/App.xaml.cs
+++ Both_TamasArpad_Lab11/App.xaml.cs
@@ -1,12 +1,14 @@
-﻿namespace Both_TamasArpad_Lab11
+﻿using Both_TamasArpad_Lab11.Data;
+
+namespace Both_TamasArpad_Lab11
 {
     public partial class App : Application
     {
+        public static ShoppingListDatabase Database { get; private set; }
         public App()
         {
-            InitializeComponent();
-
-            MainPage = new AppShell();
+            Database = new ShoppingListDatabase(new RestService()); 
+            MainPage = new NavigationPage(new ListEntryPage());
         }
     }
 }
